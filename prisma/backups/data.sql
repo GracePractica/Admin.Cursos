3824af7b5b7b1825d6e3e1fdfff8daa902dfd4c1
Axel-Saenz <Axel-Saenz@users.noreply.github.com>
2026-04-21 10:54:59 +0000

    Supabase backup

--- prisma/backups/data.sql
+++ prisma/backups/data.sql
@@ -4,7 +4,7 @@ SET session_replication_role = replica;
 -- PostgreSQL database dump
 --
 
--- \restrict 3IPmDj93gTKIlnsHUQJOY8ttfhG4idz0icZMALRQITDzAbgdMAbgJ31kYYlPGdQ
+-- \restrict nOrkoGhQEgO6yVfyLWr31qR17AmTnzWfyyod8dbBBjqKf0pLLBagKrS5xdOp5BP
 
 -- Dumped from database version 17.6
 -- Dumped by pg_dump version 17.6
@@ -21536,6 +21536,6 @@ SELECT pg_catalog.setval('"public"."puestos_id_puesto_seq"', 500184, true);
 -- PostgreSQL database dump complete
 --
 
--- \unrestrict 3IPmDj93gTKIlnsHUQJOY8ttfhG4idz0icZMALRQITDzAbgdMAbgJ31kYYlPGdQ
+-- \unrestrict nOrkoGhQEgO6yVfyLWr31qR17AmTnzWfyyod8dbBBjqKf0pLLBagKrS5xdOp5BP
 
 RESET ALL;
